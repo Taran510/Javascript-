1ee0368481ed31f11b50c794aacf0b71cb36cf40
Tarandeep <taran818246@gmail.com>
2025-03-22 11:32:27 +0000

    ..

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict" //treat all Js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(
+    3
+    +3 // code readability should be high.
+)
+
+console.log("Taran")
+
+let name = "Tania"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// data Types
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol =>
+
+// object   
+
+console.log(typeof "tania");
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
+
+
+
+
+
